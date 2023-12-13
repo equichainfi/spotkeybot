ae7b9c0a6705b6485c1010d132ac750c07c883fe
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-12-13 22:30:45 +0100

    EQC-91 starter

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { Probot } from "probot";
+
+
+
+export = (app: Probot) => {
+	app.on("pull_request.opened", async context => {
+		const prComment = context.issue({
+			body: "PRIVATE KEY SPOTTED!",
+		});
+		await context.octokit.issues.createComment(prComment);
+	});
+};
