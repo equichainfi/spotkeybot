c8d64f79f78e29eed78467b5d3c0e4b462bc37ce
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-12-13 22:47:30 +0100

    EQC-91 automatisation flow setup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import { Probot } from "probot";
 
-
-
 export = (app: Probot) => {
-	app.on("pull_request.opened", async context => {
-		const prComment = context.issue({
-			body: "PRIVATE KEY SPOTTED!",
-		});
-		await context.octokit.issues.createComment(prComment);
-	});
+    app.on("pull_request.opened", async (context) => {
+        const prComment = context.issue({
+            body: "PRIVATE KEY SPOTTED!",
+        });
+        await context.octokit.issues.createComment(prComment);
+    });
 };
