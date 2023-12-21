1a9df649f79a5991bac875fb7ca1c1a3efd6fd4e
Olivier Kobialka <113932706+OlivierKobialka@users.noreply.github.com>
2023-12-21 14:36:26 +0100

    Update temp.asm

--- temp.asm
+++ temp.asm
@@ -1,6 +1,3 @@
-          global    _start
-
-          section   .text
 _start:   mov       rax, 1                  ; system call for write
           mov       rdi, 1                  ; file handle 1 is stdout
           mov       rsi, message            ; address of string to output
