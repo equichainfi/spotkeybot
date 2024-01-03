ca2d572514cb4595d489aff5f48044c8acae7bf6
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-01-03 19:12:28 +0100

    EQC-104 fix

--- test/utils.ts
+++ test/utils.ts
@@ -1,2 +1,3 @@
 // eslint-disable-next-line quotes
 export const SUCCESS_RESPONSE = `# ✨ No Private Key found ✨\n✅ You can easily now merge this PR ✅`;
+export const TEST_REPO_URL = "https://github.com/equichainfi/repo-for-test";
